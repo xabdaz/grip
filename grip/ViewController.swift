c8f377d5a8f1538c836606226cd248c5e385293f
Asep Abdaz <85033151+xabdaz@users.noreply.github.com>
2022-06-10 23:13:45 +0700

    Initial Commit

---
+++ grip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  grip
+//
+//  Created by xabdaz on 10/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
